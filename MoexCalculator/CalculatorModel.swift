613041732cf63429b091563ebaa9f40d64100e1c
Almira Khafizova <mimirka80@gmail.com>
2024-03-12 14:25:27 +0100

    Created a Model file according to MVVM architecture. Implemented initial currency conversion logic and rates storage in CalculatorModel.

---
+++ MoexCalculator/CalculatorModel.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorModel.swift
+//  MoexCalculator
+//
+//  Created by Almira Khafizova on 12.03.24.
+//
+
+import Foundation
+
+struct CalculatorModel {
+    
+    private(set) var currencyRates: CurrencyRates = [.RUR: 1, .CNY: 12]
+ 
+    mutating func setCurrencyRates(_ currencyRates: CurrencyRates) {
+        self.currencyRates = currencyRates
+    }
+    
+    func convert(_ source: CurrencyAmount, to target: Currency) -> Double {
+        guard
+            let sourceRate = currencyRates[source.currency],
+            let targetRate = currencyRates[target]
+        else { return 0 }
+        
+        if targetRate.isZero {
+            return 0
+        } else {
+            return source.amount * sourceRate / targetRate
+        }
+    }
+}
